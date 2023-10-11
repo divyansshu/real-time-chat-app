1556abbf8dbc62a91416920ec84c9106b250382f
Divyanshu Rawat <120498621+divyansshu@users.noreply.github.com>
2023-10-12 00:36:05 +0530

    commit

--- client/src/components/chatPage.jsx
+++ client/src/components/chatPage.jsx
@@ -1,24 +1,25 @@
 import axios from "axios"
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 
 export default function ChatPage() {
+    const [chats, setChats] = useState([]);
 
-    const fetchData = async () => {
-        const data = await axios.get("http://localhost:1338/chats").then((response) => {
-            console.log(response);
+    useEffect(() => {
+         axios.get("http://localhost:1338/chats").then((response) => {
+            setChats(response.data);
         }).catch((error) => {
             console.log(error);
         });
-    }
-
-    useEffect(() => {
-        fetchData();
     }, []);
 
-    return(
-        <>
-        
-        </>
+    return (
+      <>
+        <div>
+          {chats.map((chat) => (
+            <div key={chat.id}>{chat.name}</div>
+          ))}
+        </div>
+      </>
     );
 
 }
